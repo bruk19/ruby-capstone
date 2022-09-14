bbdb9bf799f8696565573ff69a68a5b76cbb9130
bruk19 <brukteshome21@gmail.com>
2022-09-14 12:10:31 +0300

    Create list method for author

---
+++ author/author_module.rb
@@ -0,0 +1,16 @@
+require_relative './author'
+require_relative '../item'
+
+module GameModule
+    def list_authors
+        if is_empty(@authors)
+          message('No author in the catalog')
+          return
+        end
+        puts "\n"
+    
+        @authors.each_with_index do |author, i|
+          puts "#{i + 1 }) #{author.first_name} #{author.last_name}"
+        end
+    end
+end
